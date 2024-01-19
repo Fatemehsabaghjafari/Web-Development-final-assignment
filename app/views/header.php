46a969b4e0ae98ed2bf39f09e3505c8ae1f5adc0
Fatemehsabaghjafari <113588373+Fatemehsabaghjafari@users.noreply.github.com>
2024-01-19 14:18:11 +0100

    css link

--- app/views/header.php
+++ app/views/header.php
@@ -7,7 +7,7 @@
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
     <title>FS Flower Shop</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">  
-    <link rel="stylesheet" href="../views/home/style.css">
+
 </head>
 <body>
     <div class="mainDivHeader">
